9b5285a635401468bd703e79e2323e0e86a64739
dani-lm <114920218+dani-lm@users.noreply.github.com>
2023-04-10 20:33:56 +0200

    E4_maven
    Ejercicio 4 del curso de CI/CD Jenkins

---
+++ src/test/java/proyectomaven/mavenjenkins/MavenjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectomaven.mavenjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
