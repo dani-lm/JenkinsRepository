e1d07fcf6c44c04089c8566780af21d951df44c3
dani-lm <114920218+dani-lm@users.noreply.github.com>
2023-04-11 19:36:03 +0200

    e5_concatenar_1

---
+++ src/main/java/E5_concatenar/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package E5_concatenar.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+}
