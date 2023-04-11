bddc15c081d133632ac7d35f3426b0d9fff17be1
dani-lm <114920218+dani-lm@users.noreply.github.com>
2023-04-11 20:27:16 +0200

    Add files via upload

---
+++ src/test/java/E6_concatenar/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package E6_concatenar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
